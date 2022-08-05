63de94a3a3c2d2dff7050342b5d8ba32ea6c409c
josephhensh <josephhensh2000@gmail.com>
2022-08-05 19:31:56 +0100

    Update index view

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -9,7 +9,15 @@ from django.core.paginator import Paginator
 # Landing page view
 def index(request):
     animation_objects = Lib.objects.all()
-    paginator = Paginator(animation_objects,8)
+    animation_types = []
+    for animation in animation_objects:
+        if animation.anima_type not in animation_types:
+            animation_types.append(animation.anima_type) 
+    paginator = Paginator(animation_types,8)
     page = request.GET.get('page') 
-    animation_objects = paginator.get_page(page)   
-    return render(request, 'accounts/index.html', {'animation_objects': animation_objects}) 
+    animation_objects = paginator.get_page(page)  
+    context = {
+        'animation_types': animation_types,
+        'animation_objects': animation_objects        
+    } 
+    return render(request, 'accounts/index.html', context) 
