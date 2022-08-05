8fba8650a89dd87c84085b5699e84ed13a234357
josephhensh <josephhensh2000@gmail.com>
2022-08-05 18:18:49 +0100

    Feat: Add views for landing page - index

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Lib
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+
+
+# Landing page view
+def index(request):
+    animation_objects = Lib.objects.all()
+    paginator = Paginator(animation_objects,8)
+    page = request.GET.get('page') 
+    animation_objects = paginator.get_page(page)   
+    return render(request, 'accounts/index.html', {'animation_objects': animation_objects}) 
