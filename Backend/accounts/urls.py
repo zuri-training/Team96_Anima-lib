2f75ba4e1b31438244a854341cd7b6cd9de67f8b
josephhensh <josephhensh2000@gmail.com>
2022-08-08 11:58:22 +0100

    Create urls.py file for urls of our app

---
+++ Backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+app_name = 'accounts' 
+urlpatterns = [        
+    path('',views.index, name='index'),
+    path('/register', views.register, name='register')       
+   
+] 
