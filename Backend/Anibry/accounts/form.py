0b1785bb55223d8c4a2852768b85383130ed2fc3
dammycute <damilolaolawoore03@gmail.com>
2022-08-09 16:15:29 +0100

    Corrected

--- Backend/Anibry/accounts/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    class Meta:
-        model = user
-        fields = ['username', 'email', 'password']
-    username = forms.CharField(label = 'Username', maxlength = 100)
-    email = foerms.EmailField(label = 'Email')
