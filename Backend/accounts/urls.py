82de40c8007147414979e8b3ef216a89694dd93f
dammycute <damilolaolawoore03@gmail.com>
2022-08-12 20:48:37 +0100

    msg

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 app_name = 'accounts' 
 urlpatterns = [        
     path('',views.index, name='index'),
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     # path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('resetpassword', views.reset, name='resetpassword'),
