a52f10744af347116cdd784796cd291d2f7897ab
dammycute <damilolaolawoore03@gmail.com>
2022-08-12 19:20:41 +0100

    nsg

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -4,12 +4,18 @@ from django.urls import path
 app_name = 'accounts' 
 urlpatterns = [        
     path('',views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('resetpassword/', views.reset, name='resetpassword'),
-    path('forgotpassword/', views.forgot, name='forgetpassword'),      
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    # path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('resetpassword', views.reset, name='resetpassword'),
+    path('forgotpassword', views.forgot, name='forgetpassword'),      
     path('contact', views.contact, name='contact'),
-    path('documentation/', views.documentation, name='documentation'),
+    path('documentation', views.documentation, name='documentation'),
+    path('library', views.library, name='library'),
+    path('profile', views.profilepage, name='profile'),
+    path('portfolio', views.portfolio, name='portfolio'),
+    path('community', views.community, name='community'),
+    path('team', views.team, name='team'),
+
 ] 
