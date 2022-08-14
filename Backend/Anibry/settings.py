a5bb9e883151bcd8c164772780ee0a2a05c363a0
Kaptain-T <ezighi.antony@gmail.com>
2022-08-14 18:07:53 +0100

    updated a lot of template syntaxes, linked authheader and footer to some templates

--- Backend/Anibry/settings.py
+++ Backend/Anibry/settings.py
@@ -32,9 +32,13 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1:8000',
+    'anibry.zurifordummies.com',
+]
 
 
 # Application definition
